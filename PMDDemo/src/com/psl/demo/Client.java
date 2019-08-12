b2544058bcce104180f466f6964ce6edcdf186a4
ashwini <ashwini31aug@gmail.com>
2019-08-12 11:26:09 +0530

    First commit

---
+++ PMDDemo/src/com/psl/demo/Client.java
@@ -0,0 +1,18 @@
+package com.psl.demo;
+
+public class Client {
+	
+	public static void main(String[] args) {
+		Employee employee = new Employee("victor", "smith", 10);
+		Employee.setFirstName("victor");
+		Employee.setLastName("smith");
+		Employee.setHourlyRate(10);
+		
+		System.out.println(employee.toString());
+		
+		System.out.println(employee.getPayment(40));
+	
+		
+	}
+
+}
---
+++ PMDDemo/src/com/psl/demo/Employee.java
@@ -0,0 +1,60 @@
+package com.psl.demo;
+
+public class Employee {
+	private String firstName;
+	private String lastName;
+	private double hourlyRate;
+	
+	public Employee() {
+       this.firstName = "john";
+       this.lastName = "Doe";
+       this.hourlyRate = 10;
+	}
+
+	public Employee(String firstName, String lastName, double hourlyRate) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.hourlyRate = hourlyRate;
+	}
+
+	public String getFirstName() {
+	return firstName;
+	}
+
+	public static void setFirstName(String firstName) {
+		
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public static void setLastName(String lastName) {
+		
+	}
+
+	public double getHourlyRate() {
+		return hourlyRate;
+	}
+
+	public static void setHourlyRate(double hourlyRate) {
+
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [firstName=" + firstName + ", lastName=" + lastName
+				+ ", hourlyRate=" + hourlyRate + "]";
+	}
+	
+	public double getPayment(int hours)
+	{
+		return hours;
+		
+	}
+	
+	
+	
+}
+
